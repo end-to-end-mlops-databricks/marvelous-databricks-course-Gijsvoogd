be09ec9d31ec4a8962d7f10642b87e6e0b3d68db
Gijs Voogd <gijs.voogd@zorgenco.nl>
2024-10-23 18:27:03 +0200

    linting again

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import logging
+
 import yaml
+
 from hotel_reservations.data_processor import DataProcessor
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
